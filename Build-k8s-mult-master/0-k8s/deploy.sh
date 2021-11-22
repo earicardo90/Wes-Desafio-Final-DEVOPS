104bac744860299243fe4ebc66faf54041b103c6
weslley ferreira <weslley2404@gmail.com>
2021-11-22 22:40:53 +0000

    Adicionando o deployment do cluster k8s multmaster

--- Build-k8s-mult-master/0-k8s/deploy.sh
+++ Build-k8s-mult-master/0-k8s/deploy.sh
@@ -179,3 +179,88 @@ cat <<EOF > 2-provisionar-k8s-master-auto-shell.yml
     - debug:
         msg: " '{{ ps.stdout_lines }}' "
 EOF
+
+SG_IG_W=$(terraform output | grep security-group-workers-e-haproxy | awk '{print $3}' | sed -e "s/\"//g")
+
+echo $SG_IG_W
+
+cat <<EOF > security_group_master.tf
+resource "aws_security_group" "acessos_master" {
+  name        = "wes-k8s-acessos_master"
+  description = "acessos inbound traffic"
+  vpc_id = "vpc-0050d085a3350c2c9"
+
+  ingress = [
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null
+    },
+    {
+      cidr_blocks      = []
+      description      = "Libera acesso k8s_masters"
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = true
+      to_port          = 0
+    },
+     {
+       cidr_blocks      = []
+       description      = "Libera acesso k8s_workers"
+       from_port        = 0
+       ipv6_cidr_blocks = []
+       prefix_list_ids  = []
+       protocol         = "-1"
+       security_groups  = [
+         "$SG_IG_W"
+         //aws_security_group.acessos_master.id - para criar pela primeira vez é necessário comentar este bloco
+       ]
+       self             = false
+       to_port          = 0
+     },
+    {
+      cidr_blocks      = [
+        "0.0.0.0/0",
+      ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 65535
+    },
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = [],
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null,
+      description: "Libera dados da rede interna"
+    }
+  ]
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+EOF
+
+cd ../../0-terraform
+terraform apply -auto-approve
