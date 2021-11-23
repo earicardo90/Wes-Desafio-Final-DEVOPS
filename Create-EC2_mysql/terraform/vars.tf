a4b2c0912c35ad25124e04d77f3384b4e950b46b
weslley ferreira <weslley2404@gmail.com>
2021-11-23 18:42:56 +0000

    Corrigindo a pipeline de criação do mysql

---
+++ Create-EC2_mysql/terraform/vars.tf
@@ -0,0 +1,15 @@
+ variable "image_id" {
+   type    = string
+}
+
+ variable "subnet_priv_1" {
+   type    = string
+}
+
+ variable "subnet_priv_2" {
+   type    = string
+}
+
+ variable "subnet_priv_3" {
+   type    = string
+}
