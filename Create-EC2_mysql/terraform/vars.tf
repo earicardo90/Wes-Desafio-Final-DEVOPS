624c63ca3033109f669bc925c9bdef49ce5d9eeb
weslley ferreira <weslley2404@gmail.com>
2021-11-23 20:14:10 +0000

    Criando a pipeline do mysql

--- Create-EC2_mysql/terraform/vars.tf
+++ Create-EC2_mysql/terraform/vars.tf
@@ -13,3 +13,12 @@
  variable "subnet_priv_3" {
    type    = string
 }
+
+ variable "sg_workers" {
+   type    = string
+}
+variable "vpc_id" {
+  type    = string
+  default = "vpc-0050d085a3350c2c9"
+
+}
