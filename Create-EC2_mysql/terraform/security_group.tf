26a5e643222a7216073a8dca6f57b4f583172e8d
weslley ferreira <weslley2404@gmail.com>
2021-11-23 20:14:20 +0000

    Criando a pipeline do mysql

---
+++ Create-EC2_mysql/terraform/security_group.tf
@@ -0,0 +1,50 @@
+
+resource "aws_security_group" "mysql" {
+  name        = "wes_priv_mysql"
+  description = "acessos inbound traffic"
+  vpc_id = var.vpc_id 
+  ingress = [
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = null,
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null
+    },
+    {
+      description      = "Mysql from subnet"
+      from_port        = 3306
+      to_port          = 3306
+      protocol         = "tcp"
+      cidr_blocks      = []
+      security_groups  = ["${var.sg_workers}"]
+      ipv6_cidr_blocks = null,
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null
+    },
+    
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = [],
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null,
+      description: "Libera dados da rede interna"
+    }
+  ]
+
+  tags = {
+    Name = "wes_priv_mysql"
+  }
+}
