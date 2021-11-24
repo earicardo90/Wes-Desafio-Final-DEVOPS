eda865ac67c8a3adea88b20b034a700362ac8b65
weslley ferreira <weslley2404@gmail.com>
2021-11-24 15:12:25 +0000

    Corrigindo repositorio depoyment java app

--- Delivery_and_Deployment_Java_app/build_jar.sh
+++ Delivery_and_Deployment_Java_app/build_jar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd spring-web-youtube
+cd Delivery_and_Deployment_Java_app/spring-web-youtube
 mvn package -Dmaven.test.skip -DskipTests -Dmaven.javadoc.skip=true
--- Delivery_and_Deployment_Java_app/teste_jar.sh
+++ Delivery_and_Deployment_Java_app/teste_jar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd spring-web-youtube
+cd Delivery_and_Deployment_Java_app/spring-web-youtube
 mvn test
