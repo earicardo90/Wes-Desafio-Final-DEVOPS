25d3ad01e0c35652816ffd520c24a31019ea3724
weslley ferreira <weslley2404@gmail.com>
2021-11-24 15:07:33 +0000

    Corrigindo repositorio depoyment java app

--- Delivery_and_Deployment_Java_app/build_jar.sh
+++ Delivery_and_Deployment_Java_app/build_jar.sh
@@ -1,2 +1,2 @@
-cd ../spring-web-youtube
+cd spring-web-youtube
 mvn package -Dmaven.test.skip -DskipTests -Dmaven.javadoc.skip=true
--- Delivery_and_Deployment_Java_app/clone_repo_java_app.sh
+++ Delivery_and_Deployment_Java_app/clone_repo_java_app.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git clone https://github.com/torneseumprogramador/spring-web-youtube.git
+git -C Delivery_and_Deployment_Java_app/ clone https://github.com/torneseumprogramador/spring-web-youtube.git
